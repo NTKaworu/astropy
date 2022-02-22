d895904c99bc41d73dcd0eb27382533b1fcee4f0
NTKaworu <mf.lollerino@gmail.com>
2022-02-23 00:00:30 +0100

    yoy

--- main.py
+++ main.py
@@ -1,155 +1,155 @@
-from pathlib import Path
-from logzero import logger, logfile
-from sense_hat import SenseHat
-from picamera import PiCamera
-from orbit import ISS
-from time import sleep
-from datetime import datetime, timedelta
-import csv
+import configparser, time, datetime, os, sys
 
-s = SenseHat()
-cam = PiCamera()
-location = ISS.coordinates()
+sensorsNames = []
+sensorsValues = []
+path = 'output/images'
 
-base_folder = Path(__file__).parent.resolve()
-data_file = base_folder/"data.csv"
-
-sleep_duration = 30 # Seconds - Iterval between each iteration
-time_frame = 175 # Minutes - Total time frame of data gathering
+def takePicture(i, c, now):    
+    c.capture(f'{path}/{i}-{now}.jpg')
 
 
-def create_csv_file():
-    """
-    Create data file
-    """
-    with open(data_file, 'w') as g:
-        writer = csv.writer(g)
-        header = (
-            "Counter", "Date/time",
-            "Latitude", "Longitude",
-            "Temperature", "Humidity", "Pressure",
-            "Accel Pitch", "Accel Roll", "Accel Yaw",
-            "Accel X", "Accel Y", "Accel Z",
-            "Gyro Pitch", "Gyro Roll", "Gyro Yaw",
-            "Gyro X", "Gyro Y", "Gyro Z"
-            )
-        writer.writerow(header)
+def takeValues(config, s , now):
+    global sensorsValues
+    sensorsValues = []
+    if config['Sensori']['umidita'] == "True":
+        humidity = s.get_humidity()
+        sensorsValues.append(humidity)
+    if config['Sensori']['temperatura'] == "True":
+        temperature = s.get_temperature()
+        sensorsValues.append(temperature)
+    if config['Sensori']['pressione'] == "True":
+        pressure = s.get_pressure()
+        sensorsValues.append(pressure)
+    if config['Sensori']['orientamento'] == "True":
+        orientation =s.get_orientation()
+        sensorsValues.append("{pitch}, {roll}, {yaw}".format(**orientation))
+    if config['Sensori']['giroscopio'] == "True":
+        gyro_only = s.get_gyroscope()
+        sensorsValues.append("{pitch}, {roll}, {yaw}".format(**gyro_only))
+    if config['Sensori']['giroscopio_raw'] == "True":
+        gyro_raw = s.get_gyroscope_raw()
+        sensorsValues.append("{x}, {y}, {z}".format(**gyro_raw))
+    if config['Sensori']['bussola'] == "True":
+        compass_only = s.get_compass()
+        sensorsValues.append(compass_only)
+    if config['Sensori']['bussola_raw'] == "True":
+        compass_raw = s.get_compass_raw()
+        sensorsValues.append("{x}, {y}, {z}".format(**compass_raw))
+    if config['Sensori']['accelerometro'] == "True":
+        accel_only = s.get_accelerometer()
+        sensorsValues.append("{pitch}, {roll}, {yaw}".format(**accel_only))
+    if config['Sensori']['accelerometro_raw'] == "True":
+        accel_raw = s.get_accelerometer_raw()
+        sensorsValues.append("{x}, {y}, {z}".format(**accel_raw))
+    
+    sensorsValues.append(now)
+
+#    if config['Generali']['gelolocalizzazione'] == "True":
+#        sensorsValues.append(orbit.ISS.coordinates())
 
 
-def setup():
-    """
-    Application setup:
-    - create data file;
-    - create log file;
-    - setup camera resolution.
-    """
-    create_csv_file()
-    logfile(base_folder/"events.log")
-    cam.resolution = (1296, 972)
+def saveValues(t, outSheet):
+    for i in range(len(sensorsValues)):
+        outSheet.write(1+t, i, sensorsValues[i])
 
 
-def get_data(counter):
-    """
-    Collect all sensors data:
-    - ISS location;
-    - humidity;
-    - temperature;
-    - pressure;
-    - accelerometer;
-    - gyroscope.
+def unpackTuples(i):
+    new = i.split(", ")
+    k = []
+    for e in new:
+        k.append(int(e))
+    return tuple(k)
 
-    :type counter: int
-    :rtype: tuple
-    """
-    accel = s.accelerometer
-    accel_raw = s.accelerometer_raw 
-    gyro = s.gyroscope
-    gyro_raw = s.gyroscope_raw
-
-    data = (
-        counter,
-        datetime.now(),
-        location.latitude.degrees,
-        location.longitude.degrees,
-        round(s.humidity, 4),
-        round(s.temperature, 4),
-        round(s.pressure, 4),
-        accel["pitch"],
-        accel["roll"],
-        accel["yaw"],
-        accel_raw["x"],
-        accel_raw["y"],
-        accel_raw["z"],
-        gyro["pitch"],
-        gyro["roll"],
-        gyro["yaw"],
-        gyro_raw["x"],
-        gyro_raw["y"],
-        gyro_raw["z"]
-    )
-    return data
-
-
-def write_data(data):
-    """
-    Write collected sensors data to csv file
-
-    :type data: tuple
-    """
-    with open(data_file, 'a') as g:
-        writer = csv.writer(g)
-        writer.writerow(data)
-
-
-def convert(angle):
-    """
-    Utility to convert angle in to exif_angle notation
-
-    :type angle: float
-    :rtype: tuple
-    """
-    sign, degrees, minutes, seconds = angle.signed_dms()
-    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
-    return sign < 0, exif_angle
-
-
-def capture_photo(counter):
-    """
-    Get picture and tag it with additional exif data
-
-    :type counter: int
-    """
-    south, exif_latitude = convert(location.latitude)
-    west, exif_longitude = convert(location.longitude)
-
-    cam.exif_tags['GPS.GPSLatitude'] = exif_latitude
-    cam.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
-    cam.exif_tags['GPS.GPSLongitude'] = exif_longitude
-    cam.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-
-    cam.capture(f"{base_folder}/photo_{counter:03d}.jpg")
 
 
 def main():
-    setup()
+    global path
+    global sensorsNames
+    file = "config.ini"
+    config = configparser.ConfigParser()
+    config.read(file)
 
-    now = datetime.now()
-    start = datetime.now()
-    counter = 1
+    counter = 0
 
-    # Main loop
-    while now < start + timedelta(minutes=time_frame):
-        try:
-            write_data(get_data(counter))
-            capture_photo(counter)
-            logger.info(f"iteration {counter}")
-            counter += 1
-            sleep(sleep_duration)
-            now = datetime.now()
-        except Exception as e:
-            logger.error(f'{e.__class__.__name__}: {e}')
+    while os.path.exists(path):   
+        counter += 1
+        path = 'output'+(str(counter)) +'/images'
+
+    os.makedirs(path)
+
+    c = picamera.PiCamera()
+    s = sense_hat.SenseHat()
+    now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
 
-# Everything starts here
+    c.resolution = unpackTuples(config['Fotocamera']['risoluzione'])
+    c.brightness = int(config['Fotocamera']['luminosita'])
+    c.image_effect = config['Fotocamera']['effetto']
+    c.exposure_mode = config['Fotocamera']['esposizione']
+    c.awb_mode = config['Fotocamera']['bilanciamento_bianchi']
+
+    cicliNum = int(config['Generali']['numero_di_cicli'])
+    inter = int(config['Generali']['intervallo_tra_cicli'])
+
+
+    if config['Generali']['sensori'] == "True":
+        outWorkbook = xlsxwriter.Workbook('output'+(str(counter)) +'/out.xlsx')
+        outSheet = outWorkbook.add_worksheet()
+
+        if config['Sensori']['umidita'] == "True":
+            sensorsNames.append("Umidita")
+
+        if config['Sensori']['temperatura'] == "True":
+            sensorsNames.append("Temperatura")
+
+        if config['Sensori']['pressione'] == "True":
+            sensorsNames.append("Pressione")
+
+        if config['Sensori']['orientamento'] == "True":
+            sensorsNames.append("Orientamento")
+
+        if config['Sensori']['giroscopio'] == "True":
+            sensorsNames.append("Giroscopio")
+
+        if config['Sensori']['giroscopio_raw'] == "True":
+            sensorsNames.append("Giroscopio-raw")
+
+        if config['Sensori']['bussola'] == "True":
+            sensorsNames.append("Bussola")
+
+        if config['Sensori']['bussola_raw'] == "True":
+            sensorsNames.append("Bussola-raw")
+
+        if config['Sensori']['accelerometro'] == "True":
+            sensorsNames.append("Accelerometro")
+
+        if config['Sensori']['accelerometro_raw'] == "True":
+            sensorsNames.append("Accelerometro-raw")
+
+        sensorsNames.append("Date and time")
+
+        if config['Generali']['gelolocalizzazione'] == "True":
+            sensorsNames.append("ISS position")
+        
+#        for i in range(len(sensorsNames)):
+#            outSheet.write(0, i, sensorsNames[i])
+
+
+    for i in range(cicliNum):
+        if config['Generali']['fotocamera'] == "True":
+            takePicture(i, c, now)
+        if config['Generali']['sensori'] == "True":
+            takeValues(config, s, now)
+            saveValues(i, outSheet)
+        time.sleep(inter)
+    outWorkbook.close()
+
+
 if __name__ == '__main__':
-    main()
+    try:
+        import sense_hat, picamera, xlsxwriter#, orbit
+        main()
+    except ImportError as error:
+        print('\n  | Import ERROR')
+        print(f"  | No module named '{error.message[16:]}'")
+        sys.exit(1)
