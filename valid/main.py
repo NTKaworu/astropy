94dacf7352bf9a5b31f6ca9f84a54d20333ee709
NTKaworu <mf.lollerino@gmail.com>
2022-02-22 23:27:55 +0100

    yeeee

--- valid/main.py
+++ valid/main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from socket import timeout
 from logzero import logger, logfile
 from sense_hat import SenseHat
 from picamera import PiCamera
@@ -10,63 +11,125 @@ import csv
 s = SenseHat()
 cam = PiCamera()
 data_file = None
-base_folder = None
-counter = 1
+base_folder = Path(__file__).parent.resolve()
+location = ISS.coordinates()
+
+sleep_duration = 30 # Seconds - Iterval between each iteration
+time_frame = 175 # Minutes - Total time frame of data gathering
 
 
 def create_csv_file(data_file):
+    """
+    Create data file
+
+    :type data_file: string
+    """
     with open(data_file, 'w') as g:
         writer = csv.writer(g)
-        header = ("Counter", "Date/time", "Latitude", "Longitude", "Temperature", "Humidity", "Pressure")
+        header = (
+            "Counter", "Date/time",
+            "Latitude", "Longitude",
+            "Temperature", "Humidity", "Pressure",
+            "Accel Pitch", "Accel Roll", "Accel Yaw",
+            "Accel X", "Accel Y", "Accel Z",
+            "Gyro Pitch", "Gyro Roll", "Gyro Yaw",
+            "Gyro X", "Gyro Y", "Gyro Z"
+            )
         writer.writerow(header)
 
 
 def setup():
+    """
+    Application setup:
+    - create data file;
+    - create log file;
+    - setup camera resolution.
+    """
     global data_file
-    global base_folder
-    base_folder = Path(__file__).parent.resolve()
-    logfile(base_folder/"events.log")
-
-    cam.resolution = (1296, 972)
 
     data_file = base_folder/"data.csv"
     create_csv_file(data_file)
 
+    logfile(base_folder/"events.log")
 
-def get_datas(counter):
-    location = ISS.coordinates()
+    cam.resolution = (1296, 972)
+
+
+def get_data(counter):
+    """
+    Collect all sensors data:
+    - ISS location;
+    - humidity;
+    - temperature;
+    - pressure;
+    - accelerometer;
+    - gyroscope.
+
+    :type counter: int
+    :rtype: tuple
+    """
+    accel = s.accelerometer
+    accel_raw = s.accelerometer_raw 
+    gyro = s.gyroscope
+    gyro_raw = s.gyroscope_raw
+
     data = (
-    counter,
-    datetime.now(),
-    location.latitude.degrees,
-    location.longitude.degrees,
-    round(s.humidity, 4),
-    round(s.temperature, 4),
-    round(s.pressure, 4)
+        counter,
+        datetime.now(),
+        location.latitude.degrees,
+        location.longitude.degrees,
+        round(s.humidity, 4),
+        round(s.temperature, 4),
+        round(s.pressure, 4),
+        accel["pitch"],
+        accel["roll"],
+        accel["yaw"],
+        accel_raw["x"],
+        accel_raw["y"],
+        accel_raw["z"],
+        gyro["pitch"],
+        gyro["roll"],
+        gyro["yaw"],
+        gyro_raw["x"],
+        gyro_raw["y"],
+        gyro_raw["z"]
     )
     return data
 
 
 def write_data(data_file, data):
+    """
+    Write collected sensors data to csv file
+
+    :type data_file: string
+    :type data: tuple
+    """
     with open(data_file, 'a') as g:
         writer = csv.writer(g)
         writer.writerow(data)
 
 
 def convert(angle):
+    """
+    Utility to convert angle in to exif_angle notation
+
+    :type angle: float
+    :rtype: tuple
+    """
     sign, degrees, minutes, seconds = angle.signed_dms()
     exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return sign < 0, exif_angle
 
 
-def capture_photo():
-    location = ISS.coordinates()
+def capture_photo(counter):
+    """
+    Get picture and tag it with additional exif data
 
-    # Convert the latitude and longitude to EXIF-appropriate representations
+    :type counter: int
+    """
     south, exif_latitude = convert(location.latitude)
     west, exif_longitude = convert(location.longitude)
 
-    # Set the EXIF tags specifying the current location
     cam.exif_tags['GPS.GPSLatitude'] = exif_latitude
     cam.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
     cam.exif_tags['GPS.GPSLongitude'] = exif_longitude
@@ -76,23 +139,28 @@ def capture_photo():
 
 
 def main():
-    global counter
-
+    """
+    
+    """
     setup()
 
+    counter = 1
     now = datetime.now()
     start = datetime.now()
-    while (now < start + timedelta(minutes=175)):
+
+    # Main loop
+    while (now < start + timedelta(minutes=time_frame)):
         try:
-            write_data(data_file, get_datas(counter))
-            capture_photo()
+            write_data(data_file, get_data(counter))
+            capture_photo(counter)
             logger.info(f"iteration {counter}")
             counter += 1
-            sleep(30)
+            sleep(sleep_duration)
             now = datetime.now()
         except Exception as e:
             logger.error(f'{e.__class__.__name__}: {e}')
 
 
+# Everything starts here
 if __name__ == '__main__':
     main()
